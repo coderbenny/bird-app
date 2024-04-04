6a2488944b10bb798f3a6a7c918078813e26a589
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-04-04 13:55:43 +0300

    Initialized postgres database

---
+++ migrations/versions/53e7c967b612_create_table_birds.py
@@ -0,0 +1,24 @@
+"""create table birds
+
+Revision ID: 53e7c967b612
+Revises: 
+Create Date: 2024-04-04 13:42:03.646305
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '53e7c967b612'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- seed.py
+++ seed.py
@@ -1,8 +1,6 @@
 from app import app
 from models import db, Bird
 
-db.init_app(app)
-
 with app.app_context():
 
     print('Deleting existing birds...')
